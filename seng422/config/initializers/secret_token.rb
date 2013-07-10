955cf0447a7e9aa95ce4a6a578368e6b353f806a
unknown <mjpattie@a8.seng.uvic.ca>
2013-07-10 13:15:35 -0700

    Initialized rails skeleton. Also ran 'generate controller welcome index'

---
+++ seng422/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seng422::Application.config.secret_token = 'b0f8cfcf231b37c7f8d4bdde7f1eade78f4e9cf39bf7ebc73744f853899e6973d7eb8eb5723c9dc776c9fa714103d70ebf078f13e1efc31939de49296d11c1ab'
---
+++ seng422/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Seng422::Application.config.session_store :cookie_store, key: '_seng422_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Seng422::Application.config.session_store :active_record_store
