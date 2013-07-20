c22d33675332be7c37f8f4d3ec8699bcbbd69ab0
Michael Pattie <mjpattie@Michaels-MacBook-Air.local>
2013-07-20 14:33:44 -0700

    can now properly add and display items

--- seng422/app/models/item.rb
+++ seng422/app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ActiveRecord::Base
   belongs_to :checklist, :foreign_key => "checklist_id"
   attr_accessible :status, :title
+
+	before_save :default_values
+  def default_values
+    self.status ||= "unanswered"
+  end
 end
