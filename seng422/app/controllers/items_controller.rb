fe6d73ee2e54b02ec5d20d01424284ce194fad8f
Michael Pattie <mjpattie@w217-156.wireless.uvic.ca>
2013-07-22 17:39:50 -0700

    added update and commit functionality for items

--- seng422/app/controllers/items_controller.rb
+++ seng422/app/controllers/items_controller.rb
@@ -4,4 +4,20 @@ class ItemsController < ApplicationController
     @item = @checklist.items.create(params[:item])
     redirect_to checklist_path(@checklist)
   end
+	
+	def update
+		@checklist = Checklist.find(params[:checklist_id])
+		@item = @checklist.items.find(params[:id])
+		@item.update_attributes(params[:item])
+		redirect_to checklist_path(@checklist)
+	end
+
+	def destroy
+		@checklist = Checklist.find(params[:checklist_id])
+		@item  = @checklist.items.find(params[:id])
+ 		@item.destroy
+		redirect_to checklist_path(@checklist)
+	end
+
+	
 end
